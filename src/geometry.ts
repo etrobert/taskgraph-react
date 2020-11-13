315bf8b14b34ccb3eab277da37c4d3c91baa3e21
erobert <etiennerobert33@gmail.com>
2020-11-13 04:08:43 +0100

    simplify the code by using Box instead of Rect

--- src/geometry.ts
+++ src/geometry.ts
@@ -7,19 +7,45 @@ function addPoint(a: Point, b: Point): Point {
   return { x: a.x + b.x, y: a.y + b.y };
 }
 
-interface RectSize {
-  w: number;
-  h: number;
+interface Box {
+  left: number;
+  top: number;
+  right: number;
+  bottom: number;
+  width: number;
+  height: number;
 }
 
-function rectSizesEqual(a: RectSize, b: RectSize): boolean {
-  return a.w == b.w && a.h == b.h;
+interface PartialBox {
+  left: number;
+  top: number;
+  width: number;
+  height: number;
 }
 
-interface Rect extends RectSize, Point {}
-
-function rectCenter(rect: Rect): Point {
-  return { x: rect.x + rect.w / 2, y: rect.y + rect.h / 2 };
+function createBox(partialBox: PartialBox): Box {
+  return {
+    ...partialBox,
+    right: partialBox.left + partialBox.width - 1,
+    bottom: partialBox.top + partialBox.height - 1,
+  };
 }
 
-export { Point, addPoint, RectSize, rectSizesEqual, Rect, rectCenter };
+function getBoxCenter(box: Box): Point {
+  return {
+    x: box.left + box.width / 2,
+    y: box.top + box.height / 2,
+  };
+}
+
+// Testing for bottom and right is redundant as they calculated from the others
+function boxesEqual(a: Box, b: Box): boolean {
+  return (
+    a.left == b.left &&
+    a.top == b.top &&
+    a.width == b.width &&
+    a.height == b.height
+  );
+}
+
+export { Point, addPoint, Box, boxesEqual, createBox, getBoxCenter };
