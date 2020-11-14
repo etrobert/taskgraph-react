a98f16cdfdf60b2ba601653d22e4faab70cb28a6
erobert <etiennerobert33@gmail.com>
2020-11-14 17:15:57 +0100

    add localStorage backup

--- src/useGraph.ts
+++ src/useGraph.ts
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { Graph, Task } from './data';
 import { addPoint, Point } from './geometry';
 
@@ -12,14 +12,35 @@ interface UseGraph {
 
 const maxId = (graph: Graph) => Math.max(...graph.tasks.map((task) => task.id));
 
+const loadFromLocalStorage = (): Graph | null => {
+  const graphString = window.localStorage.getItem('graph');
+  // TODO validate graphString
+  return graphString && JSON.parse(graphString);
+};
+
+const saveToLocalStorage = (graph: Graph) =>
+  window.localStorage.setItem('graph', JSON.stringify(graph));
+
 const defaultInitialGraph = { tasks: [], dependencies: [] };
 
 function useGraph(initialGraph: Graph = defaultInitialGraph): UseGraph {
   const [graph, setGraph] = useState<Graph>(initialGraph);
 
-  const [nextId, setNextId] = useState(
-    initialGraph.tasks ? maxId(initialGraph) + 1 : 0
-  );
+  // Given a specific graph, returns the next id number
+  const graphNextId = (graph: Graph) => (graph.tasks ? maxId(graph) + 1 : 0);
+  const [nextId, setNextId] = useState(graphNextId(initialGraph));
+
+  // Initialize graph with localStorage
+  // TODO fix flash of initialGraph
+  useEffect(() => {
+    const graph = loadFromLocalStorage();
+    if (!graph) return;
+    setGraph(graph);
+    setNextId(graphNextId(graph));
+  }, []);
+
+  // Update localStorage on every graph modification
+  useEffect(() => saveToLocalStorage(graph), [graph]);
 
   const addTask = (name: string) => {
     setNextId((nextId) => nextId + 1);
